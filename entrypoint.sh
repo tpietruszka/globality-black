78c631a8bc9ec6fa5f0dc8ce80a8e517bc2abdee
marcglobality <marc.torrellas-socastro@globality.com>
2020-12-13 20:10:45 +0000

    use &&

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,8 +27,7 @@ elif [ "$1" = "lint" ]; then
    # Install standard linting dependencies; YMMV
    pip --quiet install \
        .[lint] flake8 flake8-print flake8-logging-format flake8-isort
-   # exec flake8 ${NAME}
-   exec globality-black ${NAME} --check
+   exec flake8 ${NAME} && globality-black ${NAME} --check
 elif [ "$1" = "typehinting" ]; then
    # Install standard type-linting dependencies
    pip --quiet install mypy
