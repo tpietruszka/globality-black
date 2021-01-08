cb5ce6d32470181fe7bc3927540408519e2fc559
marcglobality <marc.torrellas-socastro@globality.com>
2021-01-08 22:23:42 +0000

    all comlex comprehensions solved!

--- README.md
+++ README.md
@@ -37,7 +37,10 @@ graph.use(
 Explode comprehensions
 * all dict comprehensions
 * any comprehension with an if
-* list / set comprehensions where the element has a ternary operator (see examples below)
+* any comprehension with multiple for lopps (see examples below)
+* list / set comprehensions where the element:
+   - has a ternary operator (see examples below)
+   - has another comprehension
 
 For everything else, we rely on `black`. Examples:
 
@@ -53,6 +56,12 @@ For everything else, we rely on `black`. Examples:
 {"a": 3 for _ in range(4) if i < 4}
 
 ["odd" if i %% 2 == 0 else "even" for _ in range(10)]
+
+double_comp1 = [3*i*j for i in range(10) for j in range(4)]
+
+double_comp2 = [[i for i in range(7) if i < 5] for j in range(10)]
+
+double_comp3 = {i: [i for i in range(7) if i < 5] for j in range(10) if i < 2}
 ```
 
 #### After globality-black 
@@ -81,29 +90,50 @@ For everything else, we rely on `black`. Examples:
     "odd" if i %% 2 == 0 else "even" 
     for _ in range(10)
 ]
+
+double_comp1 = [
+    3 * i * j 
+    for i in range(10) 
+    for j in range(4)
+]
+
+double_comp2 = [
+    [i for i in range(7) if i < 5] 
+    for j in range(10)
+]
+
+double_comp3 = {
+    i: [i for i in range(7) if i < 5] 
+    for j in range(10) 
+    if i < 2
+}
 ```
 
+Note that in the last two comprehensions, the nested comprehensions are not exploded even though
+having an if conditional. This is a limitation of `globality-black`, but we believe not very frequent
+in everyday cases. If you would like to explode those and make `globality-black` respect it, 
+please use the feature explained next.
 
 ### Partially disable globality-black
 
 If you see some block where you don't want to apply `globality-black`, wrap it
 with `# fmt.off` and `# fmt:on` and it will be ignored. Note that this is the same syntax as
-for `black`. For example, some complex comprehensions 
-are not covered yet, so you might want to do something as:
+for `black`. For example, for readability you might want to do something as:
 
 ```
 # fmt: off
 files_to_read = [
-    (f"{key1}_{key2}", key1 + key2)
+    (f"{key1}_{key2}", key1, key2, key1 + key2)
     for key1 in range(10)
-    for key2 in range(10)
 ]
 # fmt: on
 ```
 
-See `known_failed_comprehensions` for more examples.
+Note that by as a default (same as `black`), `globality-black` will write the expression above as a
+one-liner.
 
 
 Pending / Future work
 ------------
-* Fix known_failures, mainly to cover all comprehensions
+
+All done! Please give us feedback if you find any issues
--- globality_black/comprehensions.py
+++ globality_black/comprehensions.py
@@ -22,33 +22,52 @@ def reformat_comprehension(comp_for: PythonNode):
     comp is a PythonNode with type "testlist_comp" and code
         "(3, i): "globality" for i in range(4) if i < 2"
 
-    Here we filter only the types that we explode, i.e:
-        - dict comprehension or
-        - if there is an if
+    We explode if at least one of the following requirements are met:
+        - dict comprehension
+        - there is an if
+        - there is a ternary operator
+        - there are multiple for's
 
-    We don't explode multiple for loops for now
+    We don't explode:
+        - comprehension inside of comprehension (the inner one will be kept as is)
+        - if multiple for's, the first one already explodes the rest
+        (with `set_prefix_for_all_last_children`), so they are ignored
     """
 
     assert comp_for.type == ParsoTypes.SYNC_COMP_FOR.value
 
-    is_dict = (
-        comp_for.parent.type == ParsoTypes.DICTORSETMAKER.value
-        and comp_for.parent.children[1] == ":"
-    )
+    comp = comp_for.parent
+
+    is_dict = comp.type == ParsoTypes.DICTORSETMAKER.value and comp.children[1] == ":"
     ends_with_if = comp_for.children[-1].type == ParsoTypes.COMP_IF.value
     ends_with_for = comp_for.children[-1].type == ParsoTypes.SYNC_COMP_FOR.value
-    has_ternary_operator = comp_for.parent.children[0].type == ParsoTypes.TERNARY_EXPRESSION.value
-    # TODO: add more cases to ignore or to explode. Look at known_failed_comprehensions
+    value_is_ternary_expression = comp.children[0].type == ParsoTypes.TERNARY_EXPRESSION.value
+    value_is_comprehension = find_if_value_is_comprehension(comp)
 
-    if (is_dict or ends_with_if or has_ternary_operator) and not ends_with_for:
+    parent_is_for = comp.type == ParsoTypes.SYNC_COMP_FOR.value
+    comprehension_types = (ParsoTypes.DICTORSETMAKER.value, ParsoTypes.LISTCOMP.value)
+    nested_comp = comp.parent.parent.type in comprehension_types
+
+    requirements = is_dict or ends_with_if or ends_with_for
+    requirements = requirements or value_is_ternary_expression or value_is_comprehension
+    can_be_ignored = parent_is_for or nested_comp
+
+    if requirements and not can_be_ignored:
         _reformat_comprehension(comp_for)
+
+
+def find_if_value_is_comprehension(comp):
+    value = comp.children[0]
+    for child in getattr(value, "children", []):
+        if child.type == "testlist_comp":
+            return True
+    return False
 
 
 def _reformat_comprehension(comp_for: PythonNode):
     """
     Here we do the actual reformatting
     """
-
     comp = comp_for.parent
     prefix = find_indentation_parent_prefix(comp)
     base_indent = get_indent_from_prefix(prefix)
@@ -57,13 +76,7 @@ def _reformat_comprehension(comp_for: PythonNode):
     # indent element of comp
     set_prefix(comp, new_prefix)
 
-    # indent for
-    set_prefix(comp_for, new_prefix)
-
-    # indent comprehension if's
-    for child in comp_for.children[1:]:
-        if child.type in (ParsoTypes.COMP_IF.value,):
-            set_prefix_for_all_comp_if(child, new_prefix)
+    set_prefix_for_all_last_children(comp_for, new_prefix)
 
     # set closing bracket
     set_prefix(comp.parent.children[-1], "\n" + base_indent)
@@ -83,18 +96,17 @@ def set_prefix(element: PythonNode, prefix: str):
         leaf.prefix = leaf.prefix[:last_eol_position] + prefix
 
 
-def set_prefix_for_all_comp_if(comp_if: PythonNode, prefix: str):
+def set_prefix_for_all_last_children(comp_for: PythonNode, prefix: str):
     """
-    indent if in comprehension + all other if's
-    unfortunately parso treates each new comp_if after the first one (if any) as a child of the
+    indent for in comprehension + all for and if underneath
+    unfortunately parso treates each new comp_for and comp_if after the comp_for (if any) as a child
+    of the
     previous one. Hence, we need to recursively iterate on them
 
     """
 
-    assert comp_if.type == "comp_if"
+    set_prefix(comp_for, prefix)
 
-    set_prefix(comp_if, prefix)
-
-    last_child = comp_if.children[-1]
-    if last_child.type == "comp_if":
-        set_prefix_for_all_comp_if(last_child, prefix)
+    last_child = comp_for.children[-1]
+    if last_child.type in {ParsoTypes.COMP_IF.value, ParsoTypes.SYNC_COMP_FOR.value}:
+        set_prefix_for_all_last_children(last_child, prefix)
