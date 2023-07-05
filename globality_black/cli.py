15b6ec3d27851a6e2384460bcff52cde3a253f0a
Tomasz Pietruszka <tomek.pietruszka@gmail.com>
2023-07-06 00:42:03 +0200

    bugfix

--- globality_black/cli.py
+++ globality_black/cli.py
@@ -159,7 +159,7 @@ def process_src(
     if not check_only_mode and file_mode:
         path.write_text(output_code)  # type: ignore
     if not check_only_mode and not file_mode:
-        click.echo(output_code, file=sys.stdout)
+        click.echo(output_code, file=sys.stdout, nl=False)
 
     path_str = f" {path}" if file_mode else ""
     if diff_mode:
